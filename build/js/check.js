73005092d71f9a65c9dd0d6a723ec698be43bdb1
lisit-sa <kristaliska@gmail.com>
2016-05-26 13:57:29 +0400

    remove bool

--- build/js/check.js
+++ build/js/check.js
@@ -3,9 +3,9 @@
 function getMessage(a, b) {
 
 	if (typeof a === 'boolean') {
-			if (a === true) {
+			if (a) {
 				return 'Я попал в ' + b;
-			}	else {
+		  }	else {
 				return 'Я никуда не попал';
 			};
 	}
