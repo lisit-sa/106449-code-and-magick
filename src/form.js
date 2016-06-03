59febfb91531be34c5f5eee951ea97c17f0fd74d
lisit-sa <kristaliska@gmail.com>
2016-06-03 12:40:57 +0400

    second result

--- src/form.js
+++ src/form.js
@@ -17,15 +17,14 @@
   /** Валидация формы - выносим переменные
   */
   //var getForm = document.querySelector('.review-form');
-  //var getMark = document.querySelectorAll('input[name="review-mark"]');
-  var getMarkChecked = document.querySelector('input[type=radio]:checked');
+  var getMark = document.querySelector('.review-form-group-mark');
   var getName = document.querySelector('#review-name');
   var getText = document.querySelector('#review-text');
   var labelForName = document.querySelector('.review-fields-name');
   var labelForText = document.querySelector('.review-fields-text');
   var blockError = document.querySelector('.review-fields');
   var btnSubmit = document.querySelector('.review-submit');
-  //var errorName = document.querySelector('.error-name-valid-message');
+  var errorName = document.querySelector('.error-name-valid-message');
   var errorText = document.querySelector('.error-text-valid-message');
 
   /** Присваиваем при загрузке страницы кнопке аттрибут disable
@@ -41,40 +40,57 @@
   /** Начинаем валидацию
   */
   function validate() {
-    if (getMarkChecked.value < 3) {
-      getText.required = true;
-      labelForText.style.display = 'inline-block';
-      btnSubmit.disabled = true;
-      if (getText.checkValidity()) {
+    getName.addEventListener('input', function() {
+      if (getName.value.length > 1) {
+        labelForName.style.display = 'none';
+        errorName.style.display = 'none';
+        blockError.style.display = 'none';
+        btnSubmit.disabled = false;
+      } else {
+        labelForName.style.display = 'inline-block';
+        blockError.style.display = 'inline-block';
+        errorName.innerHTML = 'Неправильный ввод';
+        btnSubmit.disabled = true;
+      }
+    });
+
+    getText.addEventListener('input', function() {
+      if (getText.value.length > 1) {
+        blockError.style.display = 'none';
+        labelForText.style.display = 'none';
+        errorText.style.display = 'none';
         btnSubmit.disabled = false;
       } else {
         errorText.innerHTML = 'Неправильный ввод';
+        blockError.style.display = 'inline-block';
+        labelForText.style.display = 'inline-block';
+        btnSubmit.disabled = true;
+        errorText.style.display = 'inline-block';
       }
-    } else {
-      getText.required = false;
-      btnSubmit.disabled = false;
-    }
+    });
+
+    getMark.addEventListener('click', function(event) {
+      var getMarkChecked = event.target;
+
+      if (getMarkChecked.value < 3) {
+        getText.required = true;
+        labelForText.style.display = 'inline-block';
+        btnSubmit.disabled = true;
+        blockError.style.display = 'inline-block';
+        if (getText.checkValidity()) {
+          btnSubmit.disabled = false;
+        } else {
+          labelForText.style.display = 'inline-block';
+          blockError.style.display = 'inline-block';
+          btnSubmit.disabled = true;
+        }
+      } else {
+        getText.required = false;
+        btnSubmit.disabled = false;
+      }
+    });
   }
 
-  getName.oninput = function() {
-    if (getName.value.length > 1) {
-      labelForName.style.display = 'none';
-    } else {
-      labelForName.style.display = 'inline-block';
-      blockError.style.display = 'inline-block';
-    }
-    validate();
-  };
+  validate();
 
-  getText.oninput = function() {
-    if (getText.value.length > 1) {
-      getText.style.display = 'none';
-    } else {
-      labelForText.style.display = 'inline-block';
-      blockError = 'inline-block';
-    }
-    validate();
-  };
 })();
-
-
