35ad1dd01547d5e27246ac98e263069338d287b0
lisit-sa <kristaliska@gmail.com>
2016-06-05 13:36:32 +0400

    result

--- src/form.js
+++ src/form.js
@@ -1,33 +1,42 @@
 'use strict';
 
 (function() {
+//находим контейнер и его элементы
   var formContainer = document.querySelector('.overlay-container');
   var formOpenButton = document.querySelector('.reviews-controls-new');
   var formCloseButton = document.querySelector('.review-form-close');
-
-  formOpenButton.onclick = function(evt) {
-    evt.preventDefault();
-    formContainer.classList.remove('invisible');
-  };
-
-  formCloseButton.onclick = function(evt) {
-    evt.preventDefault();
-    formContainer.classList.add('invisible');
-  };
-  /** Валидация формы - выносим переменные
-  */
+//находим оценки
   var form = document.querySelector('.review-form');
   var mark = form.elements.namedItem('review-mark');
-  var reviewMark = document.querySelectorAll('input[name="review-mark"]');
+//находим поля
   var name = form.querySelector('#review-name');
   var text = form.querySelector('#review-text');
   var field = form.querySelector('.field-wrap');
   var labelName = form.querySelector('.review-fields-name');
   var labelText = form.querySelector('.review-fields-text');
+//находим вспомогательные элементы
   var blockError = form.querySelector('.review-fields');
   var btnSubmit = form.querySelector('.review-submit');
   var errorName = form.querySelector('.error-name-valid-message');
   var errorText = form.querySelector('.error-text-valid-message');
+
+  formOpenButton.onclick = function(evt) {
+    evt.preventDefault();
+    Array.prototype.forEach.call(mark, function(markInArray) {
+      markInArray.addEventListener('change', reviewValidate);
+      markInArray.addEventListener('change', setErrorHidden);
+    });
+    setBlockHidden(formContainer, false);
+  };
+
+  formCloseButton.onclick = function(evt) {
+    evt.preventDefault();
+    Array.prototype.forEach.call(mark, function(markInArray) {
+      markInArray.removeEventListener('change', reviewValidate);
+      markInArray.removeEventListener('change', setErrorHidden);
+    });
+    setBlockHidden(formContainer, true);
+  };
 
   /** Присваиваем при загрузке страницы кнопке аттрибут disable
   */
@@ -58,40 +67,22 @@
   }
 
   function setErrorHidden() {
-    if(name.validity.valid) {
-      errorName.classList.add('invisible');
-    } else {
-      errorName.innerHTML = 'Пожалуйста, введите значение';
-      errorName.classList.remove('invisible');
-    }
-
-    if(text.validity.valid) {
-      errorText.classList.add('invisible');
-    } else {
-      errorText.innerHTML = 'Пожалуйста, введите значение';
-      errorText.classList.remove('invisible');
-    }
+    setBlockHidden(errorName, name.validity.valid);
+    setBlockHidden(errorText, text.validity.valid);
   }
 
   field.addEventListener('input', reviewValidate);
-  field.addEventListener('input', setErrorHidden);
-
-  for (var i = 0; i < reviewMark.length; i++) {
-    reviewMark[i].addEventListener('change', reviewValidate);
-  }
-  for (var n = 0; n < reviewMark.length; n++) {
-    reviewMark[n].addEventListener('change', setErrorHidden);
-  }
+  field.addEventListener('change', setErrorHidden);
 
   reviewValidate();
+  setErrorHidden();
 
   form.onsubmit = function() {
+    field.removeEventListener('change');
     field.removeEventListener('input');
-    reviewMark[n].removeEventListener('change');
-  };
-
-  formCloseButton.onclick = function() {
-    field.removeEventListener('input');
-    reviewMark[n].removeEventListener('change');
+    Array.prototype.forEach.call(mark, function(markInArray) {
+      markInArray.removeEventListener('change', reviewValidate);
+      markInArray.removeEventListener('change', setErrorHidden);
+    });
   };
 })();
