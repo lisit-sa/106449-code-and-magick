018b226f85506980e68ed9d5b87b8600013249a7
lisit-sa <kristaliska@gmail.com>
2016-06-26 14:01:15 +0400

    add handlers

--- src/gallery/gallery.js
+++ src/gallery/gallery.js
@@ -1,37 +1,118 @@
 'use strict';
 
 (function() {
-  var gallery = require('./gallery_utility');
-  var photoContainer = document.querySelector('.photogallery');
-  var allNumbers = document.querySelector('.preview-number-total');
+  var utilities = require('../utilities');
+
+  var galleryBlock = document.querySelector('.overlay-gallery');
+  var closeBtn = document.querySelector('.overlay-gallery-close');
+  var picturesContainer = document.querySelector('.gallery-container');
   var images = document.querySelectorAll('.photogallery-image > img');
   var currentNumber = document.querySelector('.preview-number-current');
+  var totalNumber = document.querySelector('.preview-number-total');
+  var prevLinkNode = document.querySelector('.overlay-gallery-control-left');
+  var nextLinkNode = document.querySelector('.overlay-gallery-control-right');
+  var photoGallery = document.querySelector('.photogallery');
 
   //* @param {Array.<Object>}
   var galleryPictures = [];
 
-  // функция сохранения массива из фотографий
-  function saveImages() {
-    for(var i = 0; i < images.length; i++) {
-      galleryPictures.push(images[i].getAttribute('src'));
-      images[i].dataset.number = i;
+  /** @constant {number} */
+  var KEY_CODE_ESC = 27;
+
+  var currentIndex;
+
+  var img = new Image();
+
+  photoGallery.addEventListener('click', onContainerClick);
+
+  /**
+   * @param {Array.<Object>} evt
+   */
+  function onContainerClick(evt) {
+    evt.preventDefault();
+    if (evt.target.dataset.number !== void 0) {
+      showGallery(Number(evt.target.dataset.number));
     }
-    setGallery();
   }
 
-  //Вызываем галерею
-  function setGallery() {
-    photoContainer.addEventListener('click', function(evt) {
-      evt.preventDefault();
-      var number = evt.target.getAttribute('data-number');
-      gallery.showGallery(number, galleryPictures);
-      gallery.showPicture(galleryPictures[number]);
-      currentNumber.textContent = +number + 1;
-      allNumbers.textContent = images.length;
-    });
+  /**
+   * @param {number} pictureNumber
+   */
+  function showGallery(pictureNumber) {
+
+    nextLinkNode.addEventListener('click', showNextPic);
+    prevLinkNode.addEventListener('click', showPrevPic);
+
+    closeBtn.addEventListener('click', closeGalleryBtn);
+    document.addEventListener('keydown', closeGalleryEsc);
+
+    utilities.setBlockHidden(galleryBlock);
+
+    showPicture(pictureNumber);
   }
 
-  saveImages();
-  gallery.hideGallery();
+  function showNextPic() {
+    showPicture(++currentIndex);
+  }
+
+  function showPrevPic() {
+    showPicture(--currentIndex);
+  }
+
+  /**
+   * @param {number} pictureNumber
+   */
+  function showPicture(pictureNumber) {
+    currentIndex = pictureNumber;
+
+    if (currentIndex > galleryPictures.length - 1) {
+      currentIndex = 0;
+    }
+
+    if (currentIndex < 0) {
+      currentIndex = galleryPictures.length - 1;
+    }
+
+    img.setAttribute('src', galleryPictures[currentIndex]);
+    picturesContainer.appendChild(img);
+    currentNumber.textContent = currentIndex + 1;
+    totalNumber.textContent = images.length;
+  }
+
+  /**
+   * @param {Array.<number>} nodeList
+   */
+  function collectPictures(nodeList) {
+    var i;
+    for (i = 0; i < nodeList.length; i++) {
+      galleryPictures.push(nodeList[i].getAttribute('src'));
+
+      nodeList[i].dataset.number = i;
+    }
+  }
+
+  function closeGalleryBtn() {
+    hideGallery();
+  }
+
+  function closeGalleryEsc(evt) {
+    if (evt.which === KEY_CODE_ESC) {
+      hideGallery();
+    }
+  }
+
+  function hideGallery() {
+    nextLinkNode.removeEventListener('click', showNextPic);
+
+    prevLinkNode.removeEventListener('click', showPrevPic);
+
+    closeBtn.removeEventListener('click', closeGalleryBtn);
+
+    document.removeEventListener('keydown', closeGalleryEsc);
+
+    utilities.setBlockHidden(galleryBlock);
+  }
+
+  collectPictures(images);
 
 })();
